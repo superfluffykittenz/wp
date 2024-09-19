85de11dae760303985bee8ddfb407110d02dc919
superfluffykittenz <39458680+superfluffykittenz@users.noreply.github.com>
2024-09-19 15:26:49 +1000

    Permission jammed up again

--- week9/country.php
+++ week9/country.php
@@ -1,7 +1,7 @@
 <?php 
   include("includes/db_connect.inc"); 
   $exNum = 1;
-  $title = "Wk08 Ex0" . $exNum;
+  $title = "Countries Gallery";
   include("includes/header.inc"); 
 
   $countries = mysqli_query($conn, "select * from country");
@@ -12,20 +12,47 @@
   // }
 
 ?>
-
-<table>
+<style>
+  .gallery-item {
+    border: 1px solid #666;
+    padding: 5px;
+    margin: 1px;
+    display: inline-block;
+  }
+  .gallery-item img {
+    height: 200px;
+  }
+</style>
+<!-- <table>
   <tr>
     <th>Id</th><th>Name</th><th>Description</th><th>Image</th><th>Caption</th>
   </tr>
   
 <?php  
-  while($row = mysqli_fetch_assoc($countries)) {
-    echo "<tr>";
-    foreach($row as $value ) {
-      echo "<td>$value</td>";
-    }
-    echo "</tr>";
-  }
+  // while($row = mysqli_fetch_assoc($countries)) {
+  //   echo "<tr>";
+  //   foreach($row as $value ) {
+  //     echo "<td>$value</td>";
+  //   }
+  //   echo "</tr>";
+  // }
+
 ?>
 
-</table>
+</table> -->
+<hr>
+
+<?php
+  while($row = mysqli_fetch_assoc($countries)) {
+      //pre($row);
+      echo <<<"CDATA"
+  <div class="gallery-item">
+    <h3>{$row['countryname']}</h3>
+    <img src="images/{$row['image']}" alt="{$row['description']}">
+    <p><a href="">Read More</a></p>
+  </div>
+
+CDATA;      
+  }
+
+?>
