7ccbb238c721094f465402fc9defaa287d7aa262
superfluffykittenz <superfluffykittenz@gmail.com>
2024-09-24 11:36:22 +1000

    add sql updates

--- countries/add-process.php
+++ countries/add-process.php
@@ -4,22 +4,38 @@
   pre($_POST);
   pre($_FILES);
 
-  echo "<p>Error: {$_FILES['image']['error']}</p>";
-  if ($_FILES['image']['error'] == 0 &&
-      substr($_FILES['image']['type'], 0, 6) == "image/" &&
-      $_FILES['image']['name'] != ""
-      )
-  {
-    echo ($_FILES['image']['tmp_name']);
-    echo "<hr>";
-    echo $imagePath.$_FILES['image']['name'];
-    if (move_uploaded_file($_FILES['image']['tmp_name'],$imagePath.$_FILES['image']['name'])) {
-      echo "<p>File was added to the images directory.</p>";
+/* Check if the image uploaded and is an image */  
+  if (
+    $_FILES['image']['error'] == 0 &&
+    substr($_FILES['image']['type'], 0, 6) == "image/" &&
+    !empty($_FILES['image']['name'])
+  ) {
+
+/* Create a variable for each element in $_POST, ie $name, $description, $caption */
+    foreach ($_POST as $name => $value) {
+      $$name = $value;
+    }
+
+/* Add country to the database using a prepared statement */
+    $sql = "INSERT INTO country (countryname, description, image, caption) VALUES (?,?,?,?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssss",$country, $description, $_FILES['image']['name'], $caption);
+    $stmt->execute();
+
+/* If a country was added to the database ... */
+    if ($stmt->affected_rows > 0) {
+
+/* ... try to move image from tmp folder to images folder ... */      
+      if (move_uploaded_file($_FILES['image']['tmp_name'],$imagePath.$_FILES['image']['name'])) {
+        echo "<p>File was added to the images directory.</p>";
+      } else {
+        echo "<p>File was NOT added to the images directory.</p>";
+      }    
     } else {
-      echo "<p>File was NOT added to the images directory.</p>";
+      echo "<p>Country was not added to the database, image not uploaded.</p>";
     }
   } else {
-    echo "<p>There is a problem with the file. Click here to go back.</p>";
+    echo "<p>There is a problem with the image file.</p>";
   }
 
 ?>
